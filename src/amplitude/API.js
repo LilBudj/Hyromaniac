42389df8a7309b3b113d6f961fd30d6ff863bffc
LilBudj <kinich2001@gmail.com>
2020-03-20 21:03:20 +0300

    api implemented [1]

--- src/amplitude/API.js
+++ src/amplitude/API.js
@@ -7,8 +7,24 @@ const APIKey = '29a7f14698a3dc6e0d2ee5ef3992f72c';
 const secretKey = '202a69b2dc6f1687700227df81ab2e2d';
 const projectId = '268470';
 
-export const eventAPI = {
-  mountEvent(){
+const eventConstructor = (name) => {
+    return {
+        "api_key": APIKey,
+        "event": {
 
+        }
+    }
+};
+
+export const eventAPI = {
+  mountEvent(event){
+      instance.post('/', {api_key: APIKey}).then(response => {
+          return response
+      })
+  },
+  clickEvent(event){
+      instance.post('/', {api_key: APIKey}).then(response => {
+          return response
+      })
   }
 };
